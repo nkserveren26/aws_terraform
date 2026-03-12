67a0fec6cf4042ef7703454a7a770700d8a428d5
nkserveren26 <norinoritoshi@outlook.jp>
2026-03-12 22:26:18 +0900

    EC2 module作成。

---
+++ modules/ec2/main.tf
@@ -0,0 +1,20 @@
+data "aws_ssm_parameter" "amazonlinux_2023" {
+  name = "/aws/service/ami-amazon-linux-latest/al2023-ami-kernel-6.1-x86_64" # x86_64
+}
+
+resource "aws_instance" "ec2" {
+  ami           = "ami-0d52744d6551d851e"
+  instance_type = var.instance_type
+
+  subnet_id = var.subnet_id
+
+  vpc_security_group_ids = [
+    var.sg_id
+  ]
+
+  iam_instance_profile = var.instance_profile
+
+  tags = {
+    Name = var.instance_name
+  }
+}
