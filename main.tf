d13c7c5186052d3cf7b7ea522a77d419a2531d08
nkserveren26 <norinoritoshi@outlook.jp>
2026-03-12 22:32:01 +0900

    main.tf修正。IAMとEC2のモジュールを呼び出す処理を追加。

--- main.tf
+++ main.tf
@@ -3,3 +3,19 @@ module "vpc" {
 
   vpc_cidr = "10.0.0.0/16"
 }
+
+module "iam" {
+  source = "./modules/iam"
+}
+
+module "ec2" {
+  source = "./modules/ec2"
+
+  instance_type = var.instance_type
+  instance_name = var.instance_name
+
+  subnet_id = module.vpc.subnet_id
+  sg_id     = module.vpc.security_group_id
+
+  instance_profile = module.iam.instance_profile
+}
