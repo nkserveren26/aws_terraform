e1f9a377199e8d3ba33b0f93f5025cc9500c1f9a
nkserveren26 <norinoritoshi@outlook.jp>
2026-03-14 23:49:45 +0900

    IAM module修正。Session Managerで接続するのに必要な権限を付与。

--- modules/iam/main.tf
+++ modules/iam/main.tf
@@ -14,6 +14,11 @@ resource "aws_iam_role" "ec2_role" {
   })
 }
 
+resource "aws_iam_role_policy_attachment" "ssm" {
+  role       = aws_iam_role.ec2_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
+
 resource "aws_iam_instance_profile" "profile" {
   name = "ec2-profile"
   role = aws_iam_role.ec2_role.name
