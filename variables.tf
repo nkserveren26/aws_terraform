9f8e35e305179ee10e11fa58a391ba4a089307b1
nkserveren26 <norinoritoshi@outlook.jp>
2026-03-18 21:21:28 +0900

    os_typeの変数を追加。

--- variables.tf
+++ variables.tf
@@ -9,3 +9,18 @@ variable "instance_type" {
 variable "instance_name" {
   default = "selfservice-ec2"
 }
+
+variable "os_type" {
+  description = "OS type"
+  type        = string
+
+  validation {
+    condition = contains([
+      "amazonlinux2023",
+      "ubuntu",
+      "windows"
+    ], var.os_type)
+
+    error_message = "Invalid OS type."
+  }
+}
