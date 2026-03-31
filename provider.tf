2b694e7067d4f95dd270223093a0c39fa9f2343f
nkserveren26 <norinoritoshi@outlook.jp>
2026-03-31 15:08:35 +0900

    provider.tf修正。backend S3 の定義を追加。#1

--- provider.tf
+++ provider.tf
@@ -1,3 +1,13 @@
 provider "aws" {
   region = var.aws_region
 }
+
+terraform {
+  backend "s3" {
+    bucket         = "my-terraform-state-bucket-12345-kn"
+    key            = "ec2/terraform.tfstate"
+    region         = "ap-northeast-1"
+    encrypt        = true
+    dynamodb_table = "terraform-lock"
+  }
+}
